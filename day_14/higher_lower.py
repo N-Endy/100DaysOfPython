fb55b8ffc76388e897c0c20c082ffd25c5c6c0bf
N-Endy <okafornel@gmail.com>
2023-09-18 15:58:19 +0100

    Add new files for game data and ascii art

---
+++ day_14/higher_lower.py
@@ -0,0 +1,11 @@
+# load ascii art for game
+# load prompt for compare and Against, A and B
+# Prompt for pick
+# IF wrong, game ends
+# ELSE
+    # score is updated
+    # prompt message
+    # A becomes B
+    # new data loaded for B
+    # prompt choice again
+
