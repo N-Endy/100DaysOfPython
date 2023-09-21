26d9f548e5b2e4be2b55cc044d6a8b350c1432b0
N-Endy <okafornel@gmail.com>
2023-09-21 16:55:57 +0100

    Add day_16 folder with finished code coffe_maker OOP style

---
+++ day_16/test.py
@@ -0,0 +1,18 @@
+# import turtle
+
+# timmy = turtle.Turtle()
+# timmy.shape("turtle")
+# timmy.color("yellow")
+
+# my_screen = turtle.Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+print(table)
