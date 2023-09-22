c748d47162a9026a8117eb697fa965a9d994300a
N-Endy <okafornel@gmail.com>
2023-09-22 13:57:03 +0100

    Complete quiz

--- day_17/main.py
+++ day_17/main.py
@@ -9,6 +9,7 @@ for data in question_data:
 
 quiz = QuizBrain(question_bank)
 
-still_has_questions = quiz.still_has_questions()
-while still_has_questions:
+while quiz.still_has_questions():
     quiz.next_question()
+
+quiz.game_end()
--- day_17/quiz_brain.py
+++ day_17/quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
 
     def still_has_questions(self):
@@ -12,4 +13,21 @@ class QuizBrain:
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_input = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
-        return user_input
+        self.check_answer(user_input, current_question.answer)
+    
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+            print(f"Your current score {self.score}")
+        else:
+            print("That's wrong")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
+
+
+    def game_end(self):
+        print("You have completed the quiz")
+        print(f"Your final score was: {self.score}/{len(self.question_list)}")
