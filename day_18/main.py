44cde741ed19a93882e92d2172bb22bc9d81ac2e
N-Endy <okafornel@gmail.com>
2023-09-28 12:36:44 +0100

    Add directory for day 18

---
+++ day_18/main.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+import tkinter as TK
+
+timmy = Turtle()
+timmy.shape("turtle")
+color = ["red", "green", "blue", "yellow"]
+
+# for i in color:
+#     timmy.color(i)
+#     timmy.forward(100)
+#     timmy.right(90)
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
