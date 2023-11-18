cbe4a379b8c2fbe8ec3c475e2dd15a374a19880a
nelson@wiretooth.com <nelson@wiretooth.com>
2023-11-18 19:51:06 +0100

    Create Scoroebaord class with functionality

--- day_20/score_board.py
+++ day_20/score_board.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 class ScoreBoard(Turtle):
     def __init__(self):
@@ -6,6 +8,22 @@ class ScoreBoard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 270)
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.goto(0, 260)
         self.hideturtle()
+        self.update_scoreboard()
+        
+        
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        
+        
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+        
+    def game_over(self):
+        self.color("white")
+        self.goto(0, 0)
+        self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
+        
--- day_20/snake.py
+++ day_20/snake.py
@@ -16,11 +16,16 @@ class Snake:
         
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment()
+            
+            
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+        
             
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -28,6 +33,7 @@ class Snake:
             new_y = self.segments[seg_num -1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+        
         
     def up(self):
         if self.head.heading() != DOWN:
