d1bc0cb80628c2d8bac3e1cb5161217f8c3bb99d
nelson@wiretooth.com <okafornel@gmail.com>
2023-11-21 16:36:57 +0100

    Add codes for Car and Player classes. Add to main

--- tutle-crossing/car_manager.py
+++ tutle-crossing/car_manager.py
@@ -1,7 +1,30 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        self.all_cars = []
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.color(random.choice(COLORS))
+            new_car.shapesize(1, 2)
+            new_car.penup()
+            new_car.setheading(180)
+            new_car.goto(300, random.randint(-250, 250))
+            self.all_cars.append(new_car)
+        
+    def move_cars(self):
+        for car in self.all_cars:
+            car.forward(MOVE_INCREMENT)
+
+# create turtles
+# come froom different spots (x always 300)
+# move turtles
--- tutle-crossing/main.py
+++ tutle-crossing/main.py
@@ -4,11 +4,24 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+# Initialize screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+screen.listen()
+
+# Initialize player
+player = Player()
+screen.onkey(player.move, "Up")
+
+# Initialize car
+car_manager = CarManager()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
+    car_manager.create_car()
+    car_manager.move_cars()
