ba7798b63e293e22e0d662d85638e386f2da5c4b
nelson@wiretooth.com <okafornel@gmail.com>
2023-11-22 20:31:53 +0100

    Add update highscore for each game restart

--- day_20/score_board.py
+++ day_20/score_board.py
@@ -6,7 +6,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_highscore()
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -26,8 +26,18 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.save_highscore()
         self.score = 0
         self.update_scoreboard()
+        
+    def read_highscore(self):
+        with open("highscore.txt") as file:
+            num = int(file.read())
+            return num
+        
+    def save_highscore(self):
+        with open("highscore.txt", "w") as file:
+            file.write(str(self.high_score))
         
     # def game_over(self):
     #     self.color("white")
