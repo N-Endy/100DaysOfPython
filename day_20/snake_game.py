93ca3f1252c32a169a3fbe5ccd6c77545ab3e10e
nelson@wiretooth.com <okafornel@gmail.com>
2023-11-22 19:46:11 +0100

    Update game over logic to restart game with a highscore

--- day_20/snake_game.py
+++ day_20/snake_game.py
@@ -36,14 +36,14 @@ while game_is_on:
         
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score_board.game_over()
+        score_board.reset()
+        snake.reset()
         
     # Detect collision with tail
     for tail in snake.segments[1:]:
         if snake.head.distance(tail) < 10:
-            game_is_on = False
-            score_board.game_over()
+            score_board.reset()
+            snake.reset()
 
 
 
