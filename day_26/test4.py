f7c58ca61f73ad4249fe9481f1185ddc0d2d9180
nelson@wiretooth.com <okafornel@gmail.com>
2023-11-30 18:21:09 +0100

    Remove redundant space in the split method

--- day_26/test4.py
+++ day_26/test4.py
@@ -1,4 +1,4 @@
 sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
-result = {word:len(word) for word in sentence.split(" ")}
+result = {word:len(word) for word in sentence.split()}
 
 print(result)
