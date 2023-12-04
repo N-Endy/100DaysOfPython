f354d75a2728de502e8b87539f876eda1a28b148
nelson@wiretooth.com <okafornel@gmail.com>
2023-12-04 23:16:29 +0100

    Add logic for timer mechanism for different time

--- day_28/pomodoro-start/main.py
+++ day_28/pomodoro-start/main.py
@@ -5,15 +5,24 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(WORK_MIN * 60)
+    global reps
+    reps += 1
+    
+    if reps % 8 == 0:
+        count_down(LONG_BREAK_MIN * 60)
+    elif reps % 2 == 0:
+        count_down(SHORT_BREAK_MIN * 60)
+    else:
+        count_down(WORK_MIN * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
@@ -25,6 +34,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{minute}:{second}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
