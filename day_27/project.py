3271a9b291efea2c1d2b625b383de865f7882fff
nelson@wiretooth.com <okafornel@gmail.com>
2023-12-04 21:25:33 +0100

    Add logic to convert miles to km

--- day_27/project.py
+++ day_27/project.py
@@ -10,7 +10,7 @@ input.grid(column=1, row=0)
 
 label_miles = tkinter.Label(text="Miles")
 label_equal = tkinter.Label(text="is equal to")
-label_convert = tkinter.Label(text="32")
+label_convert = tkinter.Label(text="0")
 label_km = tkinter.Label(text="Km")
 
 label_miles.grid(column=2, row=0)
@@ -18,7 +18,12 @@ label_equal.grid(column=0, row=1)
 label_convert.grid(column=1, row=1)
 label_km.grid(column=2, row=1)
 
-button = tkinter.Button(text="Calculate")
+def calculate():
+    output = input.get()
+    convert = round(float(output) * 1.6)
+    label_convert.config(text=convert)
+
+button = tkinter.Button(text="Calculate", command=calculate)
 button.grid(column=1, row=2)
 
 
