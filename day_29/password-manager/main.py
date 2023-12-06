0691bf66ac85f04be3a24fac598db57bc8cc48ba
nelson@wiretooth.com <okafornel@gmail.com>
2023-12-06 17:09:49 +0100

    Add code for layout for Password Manager

--- day_29/password-manager/main.py
+++ day_29/password-manager/main.py
@@ -4,14 +4,37 @@ import tkinter
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# Window setup
 window = tkinter.Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
+# Canvas and Image
 canvas = tkinter.Canvas(width=200, height=200, highlightthickness=0)
 photo_img = tkinter.PhotoImage(file="./logo.png")
 canvas.create_image(100, 100, image=photo_img)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+# Labels, Inputs and Buttons
+website = tkinter.Label(text="Website:")
+website_input = tkinter.Entry(width=35)
+website.grid(column=0, row=1)
+website_input.grid(column=1, row=1, columnspan=2)
+
+email = tkinter.Label(text="Email/Usrename:")
+email_input = tkinter.Entry(width=35)
+email.grid(column=0, row=2)
+email_input.grid(column=1, row=2, columnspan=2,)
+
+password = tkinter.Label(text="Password:")
+password_input = tkinter.Entry(width=21)
+generate_password = tkinter.Button(text="Generate Password")
+password.grid(column=0, row=3)
+password_input.grid(column=1, row=3)
+generate_password.grid(column=2, row=3)
+
+add_button = tkinter.Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 
 
