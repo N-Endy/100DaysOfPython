7357bc9911eafaae9d6b08c7178f7a4f89568d31
nelson@wiretooth.com <okafornel@gmail.com>
2023-12-06 17:26:11 +0100

    Change size of buttons and Input

--- day_29/password-manager/main.py
+++ day_29/password-manager/main.py
@@ -7,7 +7,7 @@ import tkinter
 # Window setup
 window = tkinter.Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # Canvas and Image
 canvas = tkinter.Canvas(width=200, height=200, highlightthickness=0)
@@ -17,23 +17,23 @@ canvas.grid(column=1, row=0)
 
 # Labels, Inputs and Buttons
 website = tkinter.Label(text="Website:")
-website_input = tkinter.Entry(width=35)
+website_input = tkinter.Entry(width=42)
 website.grid(column=0, row=1)
 website_input.grid(column=1, row=1, columnspan=2)
 
 email = tkinter.Label(text="Email/Usrename:")
-email_input = tkinter.Entry(width=35)
+email_input = tkinter.Entry(width=42)
 email.grid(column=0, row=2)
 email_input.grid(column=1, row=2, columnspan=2,)
 
 password = tkinter.Label(text="Password:")
-password_input = tkinter.Entry(width=21)
-generate_password = tkinter.Button(text="Generate Password")
+password_input = tkinter.Entry(width=24)
+generate_password = tkinter.Button(text="Generate Password", width=14)
 password.grid(column=0, row=3)
 password_input.grid(column=1, row=3)
 generate_password.grid(column=2, row=3)
 
-add_button = tkinter.Button(text="Add", width=36)
+add_button = tkinter.Button(text="Add", width=40)
 add_button.grid(column=1, row=4, columnspan=2)
 
 
