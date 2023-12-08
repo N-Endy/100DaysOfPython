9dbed528bb26b6f47c423b710bda6a1268d2a414
nelson@wiretooth.com <okafornel@gmail.com>
2023-12-09 00:05:46 +0100

    Add flip_card fuction

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -11,9 +11,21 @@ data = pandas.read_csv("./data/french_words.csv")
 word_list = [{"French": row.French, "English": row.English} for (index, row) in data.iterrows()]
 
 def next_card():
+    global flip_timer
+    window.after_cancel(flip_timer)
     word_choice = random.choice(word_list)
-    canvas.itemconfig(title_text, text="French")
-    canvas.itemconfig(word_text, text=word_choice["French"])
+    canvas.itemconfig(title_text, text="French", fill="black")
+    canvas.itemconfig(word_text, text=word_choice["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=card_front)
+    flip_timer = window.after(3000, func=flip_card)
+    return word_choice
+    
+    
+def flip_card():
+    english_word = next_card()
+    canvas.itemconfig(title_text, text="English", fill="white")
+    canvas.itemconfig(word_text, text=english_word["English"], fill="white")
+    canvas.itemconfig(canvas_image, image=card_back)
 
 
 # ----------------- Window Ui ------------------------
@@ -21,12 +33,16 @@ window = tkinter.Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
+
 '''
 Create canvas, create photo image, load photo image and text on canvas. set to grid
 '''
 canvas = tkinter.Canvas(width=800, height=526)
 card_front = tkinter.PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front)
+card_back = tkinter.PhotoImage(file="./images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front)
 title_text = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 word_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
