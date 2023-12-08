f5fe7bd5cb1c4f887cbcbb79ffa0ef73f3a63d47
nelson@wiretooth.com <okafornel@gmail.com>
2023-12-08 23:35:29 +0100

    Create next_card function too handle word change

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -1,8 +1,22 @@
 import tkinter
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-# ----------------- WIndow Ui ------------------------
+# ------------------ Generate Words --------------------
+
+data = pandas.read_csv("./data/french_words.csv")
+# word_list = data.to_dict(orient="records")
+word_list = [{"French": row.French, "English": row.English} for (index, row) in data.iterrows()]
+
+def next_card():
+    word_choice = random.choice(word_list)
+    canvas.itemconfig(title_text, text="French")
+    canvas.itemconfig(word_text, text=word_choice["French"])
+
+
+# ----------------- Window Ui ------------------------
 window = tkinter.Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -13,20 +27,20 @@ Create canvas, create photo image, load photo image and text on canvas. set to g
 canvas = tkinter.Canvas(width=800, height=526)
 card_front = tkinter.PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+title_text = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_button_img = tkinter.PhotoImage(file="./images/wrong.png")
-wrong_button = tkinter.Button(image=wrong_button_img, highlightthickness=0)
+wrong_button = tkinter.Button(image=wrong_button_img, highlightthickness=0, command=next_card)
 right_button_img = tkinter.PhotoImage(file="./images/right.png")
-right_button = tkinter.Button(image=right_button_img, highlightthickness=0)
+right_button = tkinter.Button(image=right_button_img, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 right_button.grid(column=1, row=1)
 
 
-
+next_card()
 
 
 
