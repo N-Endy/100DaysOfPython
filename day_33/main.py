3a3c23f76d2059d000c2b68a8100f28d6380d8f3
nelson@wiretooth.com <okafornel@gmail.com>
2024-01-12 18:36:40 +0100

    Futher practice with ISS endpoint

--- day_33/main.py
+++ day_33/main.py
@@ -1,11 +1,30 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 6.524379
+MY_LONG = 3.379206
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+parameter = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameter)
+response.raise_for_status()
 data = response.json()
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
 
-iss_position = (longitude, latitude)
-
-print(iss_position)
+sun_data = (sunrise, sunset)
+print(sun_data)
